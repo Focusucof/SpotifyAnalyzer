d0fdb481d8459fd37e47c2ea76627d3b52403f35
Focusucof <imthefirstfocus@gmail.com>
2021-02-18 14:03:03 -0500

    made client id and client secret keys variables that are processed from a .env file

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from pprint import pprint
+from dotenv import load_dotenv
+import os
+import json
+from collections import Counter
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="",
-                                                           client_secret=""))
+load_dotenv()
+CLIENTID = os.getenv('CLIENTID')
+CLIENTSECRET = os.getenv('CLIENTSECRET')
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=CLIENTID,
+    client_secret=CLIENTSECRET
+    )
+)
 
 pl_id = 'spotify:playlist:7pEHQIFPFnELcHSIYSyzsP'
 offset = 0
 
+
+
 while True:
-    response = sp.playlist_items(pl_id,
-                                 offset=offset,
-                                 fields='items.track.artists.name,total',
-                                 additional_types=['track'])
+    response = sp.playlist_items(
+        pl_id,
+        offset=offset,
+        fields='items.track.artists.name,total',
+        additional_types=['track']
+    )
     
     if len(response['items']) == 0:
         break
-    
-    pprint(response['items'])
+
+    pprint(response)
     offset = offset + len(response['items'])
     print(offset, "/", response['total'])
